84ad7dda2879bdd9837928c52f636de569a11459
wendy <274723525@qq.com>
2018-01-27 14:26:07 +0800

    增加京东金融项目

---
+++ app/js/core/btn.vue
@@ -0,0 +1,27 @@
+<template lang="html">
+<!-- 我们先默认用btnClass类，如果它满足不了我们需求的时候，我们自定义类名cname去覆盖btnClass样式 -->
+  <div :class="[btnClass,cname]">
+    <slot/>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    cname: {
+      type: String,
+      default: ''
+    }
+  },
+  data () {
+    return {
+      btnClass: 'btn',
+    }
+  }
+}
+</script>
+<style lang="scss">
+@import "../../css/element.scss";
+.btn{
+  @include btn;
+}
+</style>
---
+++ app/js/test.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+    <div :class="$style.test">
+        <h1>{{ title }}</h1>
+        <p>abcdef</p>
+    </div>
+</template>
+
+<script>
+export default {
+    data() {
+        return {
+            title: "abcd",
+        }
+    },
+}
+</script>
+
+<style lang="scss" module>
+  .test{
+    color: rgb(255, 94, 0);
+    h1{
+      font-size:40px;
+    }
+  }
+</style>
